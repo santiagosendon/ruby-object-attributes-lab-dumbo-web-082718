098134f5f0acae09acef820286c3837a3c1389cc
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-29 16:00:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  
+ def initialize
+    @name = "fido"
+    @breed = "Beagle"
+  end
+    
+  def name 
+    @name
+  end
+  
+  def name=(name)
+      @name = name
+      
+  def breed
+    @breed
+end
+
+ def breed=(breed)
+   @breed = breed
+end
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def initialize
+ @name = "Beyonce"
+ @job = "Singer"
+end
+
+ def name
+   @name
+ end
+ 
+ def name=(name)
+   @name = name
+ end
+ 
+ def job
+   @job
+ end
+ 
+ def job=(job)
+   @job = job
+ end
+ end
+ 
+ 
+ 
